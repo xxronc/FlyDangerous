648ee597586d0688ec394ad347ca2096802c49f6
Aaron <psyac14@nottingham.ac.uk>
2025-03-19 16:26:54 +0000

    Added Mendi Folders

--- Assets/Scripts/Core/Game.cs
+++ Assets/Scripts/Core/Game.cs
@@ -488,7 +488,7 @@ namespace Core {
             MenuFirstRun = false;
             var mapMagic = FindObjectOfType<MapMagicObject>();
             if (mapMagic) {
-                foreach (var terrainTile in mapMagic.tiles.All()) terrainTile.StopGenerate();
+                foreach (var terrainTile in mapMagic.tiles.All()) terrainTile.Stop();
                 mapMagic.enabled = false;
             }
 
